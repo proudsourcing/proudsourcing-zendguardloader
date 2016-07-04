b11ddfb4caf9fff65ddded3821a43ad931d363ef
root <root@bigboss.manitu.net>
2016-07-04 18:11:38 +0200

    update latest

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,6 +18,13 @@ class ps_zendguardloader (
 	    subscribe => File["${apache_modules_dir}"]
 	}
 	
+	if $php_version == "5.5" or $php_version == "5.6" {
+		file { "${apache_modules_dir}opCache-php-${php_version}-linux-glibc23-x86_64.so":
+			ensure => present,
+	    	source => "puppet:///modules/ps_zendguardloader/opCache-php-${php_version}-linux-glibc23-x86_64.so",
+		}
+	}
+	
 	file { "${apache_php_dir}conf.d/ps_zendguardloader.ini":
 		ensure => present,
 	    content => template("ps_zendguardloader/ps_zendguardloader.ini.erb"),
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,5 +2,5 @@ class ps_zendguardloader::params
 {
     $apache_modules_dir		= '/etc/apache2/modules/zgl/'
     $apache_php_dir			= '/etc/php5/apache2/'
-    $php_version			= '5.6  
+    $php_version			= '5.6'
 }
